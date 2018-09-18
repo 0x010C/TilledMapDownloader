1a94ef9fba711537c317ada98ceb613556b274d3
Antoine Lamielle <gh@0x010c.fr>
2018-09-18 22:44:59 +0200

    Split all the downloading/processing tasks outside the main thread

--- tilledMapDownloader.py
+++ tilledMapDownloader.py
@@ -17,7 +17,7 @@ import os
 from mainWindow import Ui_MainWindow
 from preferences import Ui_PreferencesDialog
 from PyQt5 import QtCore, QtGui, QtWidgets
-from worker import Worker
+from worker import TilesGenerator, TilesDownloader, TilesMerger
 
 
 #Custom slots
@@ -25,29 +25,64 @@ def openHelp(self):
     QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://github.com/0x010C/TilledMapDownloader"))
 Ui_MainWindow.openHelp = openHelp
 
+
 def updateStatus(self, progress, message):
     self.statusBar.setProperty("value", progress*100)
     self.statusMessage.setText(message)
+    if progress == 0.8:
+        self.setupMergeStep()
+    elif progress == 1:
+        self.setupFinishedStep()
 Ui_MainWindow.updateStatus = updateStatus
-
 
 def startProcess(self):
     if self.urlInput.text() != "":
         filename = QtWidgets.QFileDialog.getSaveFileName(None, "TilledMapDownloader", "", "Images (*.png *.jpg *.jpeg)", "", QtWidgets.QFileDialog.DontUseNativeDialog)
         if filename[0]:
             self.startButton.setDisabled(True)
-            filename = os.path.normpath(filename[0])
+            self.filename = os.path.normpath(filename[0])
             if uiPreferences.keepTilesInput.isChecked():
-                directory = os.path.join(os.path.split(filename)[0], "tiles")
-                if not os.path.exists(directory):
-                    os.makedirs(directory)
+                self.directory = os.path.join(os.path.split(self.filename)[0], "tiles")
+                if not os.path.exists(self.directory):
+                    os.makedirs(self.directory)
             else:
-                directory = QtCore.QTemporaryDir().path()
-            work = Worker(self.xMinInput.value(), self.xMaxInput.value(), self.yMinInput.value(), self.yMaxInput.value(), self.urlInput.text(), directory, filename, self.updateStatus, uiPreferences.threadsInput.value())
-            self.startButton.setEnabled(True)
-
-
+                self.directory = QtCore.QTemporaryDir().path()
+            self.setupDownloadStep()
 Ui_MainWindow.startProcess = startProcess
+
+def setupDownloadStep(self):
+    self.min_x = self.xMinInput.value()
+    self.max_x = self.xMaxInput.value()
+    self.min_y = self.yMinInput.value()
+    self.max_y = self.yMaxInput.value()
+    base_url = self.urlInput.text()
+    nb_threads = uiPreferences.threadsInput.value()
+
+    tiles_generator = TilesGenerator(self.min_x, self.max_x, self.min_y, self.max_y, self.directory)
+
+    i = 0
+    for y in range(self.min_y, self.max_y+1):
+        directory_y = os.path.join(self.directory, str(y))
+        if not os.path.exists(directory_y):
+            os.makedirs(directory_y)
+
+    self.threads = []
+    for i in range(0, nb_threads):
+        thread = TilesDownloader(tiles_generator, base_url, self.directory)
+        thread.progress.connect(self.updateStatus)
+        thread.start()
+        self.threads.append(thread)
+Ui_MainWindow.setupDownloadStep = setupDownloadStep
+
+def setupMergeStep(self):
+    self.merger = TilesMerger(self.min_x, self.max_x, self.min_y, self.max_y, self.directory, self.filename)
+    self.merger.progress.connect(self.updateStatus)
+    self.merger.start()
+Ui_MainWindow.setupMergeStep = setupMergeStep
+
+def setupFinishedStep(self):
+    self.startButton.setDisabled(False)
+Ui_MainWindow.setupFinishedStep = setupFinishedStep
 
 
 if __name__ == "__main__":
--- worker.py
+++ worker.py
@@ -10,7 +10,8 @@ import os
 from PIL import Image
 import requests
 import shutil
-import threading
+from threading import Lock
+from PyQt5 import QtCore
 from subprocess import call
 
 #Parameters
@@ -18,17 +19,15 @@ tile_width = 256  #TODO: Get this value automaticaly
 tile_height = 256
 
 #Threading vars
-lock = threading.Lock()
+lock = Lock()
 
 class TilesGenerator():
-    def __init__(self, min_x, max_x, min_y, max_y, tiles_directory, status_callback):
-        self.daemon = True
+    def __init__(self, min_x, max_x, min_y, max_y, tiles_directory):
         self.min_x = min_x
         self.max_x = max_x
         self.min_y = min_y
         self.max_y = max_y
         self.tiles_directory = tiles_directory
-        self.status_callback = status_callback
 
         self.nb_total_tiles = (max_x-min_x+1)*(max_y-min_y+1)
 
@@ -42,16 +41,10 @@ class TilesGenerator():
                 break
             elif self.x < self.max_x:
                 self.x = self.x+1
-                self.index = self.index+1
-                self.status_callback((0.6*self.index/self.nb_total_tiles), "Téléchargement des tuiles {0}/{1}".format(self.index, self.nb_total_tiles))
-                print('\b'*(len(str(self.nb_total_tiles))*2+27)+"[{0}/{1}] Download".format(self.index, self.nb_total_tiles), end="")
                 sys.stdout.flush()
             elif self.y < self.max_y:
                 self.y = self.y+1
                 self.x = self.min_x
-                self.index = self.index+1
-                self.status_callback((0.6*self.index/self.nb_total_tiles), "Téléchargement des tuiles {0}/{1}".format(self.index, self.nb_total_tiles))
-                print('\b'*(len(str(self.nb_total_tiles))*2+27)+"[{0}/{1}] Download".format(self.index, self.nb_total_tiles), end="")
                 sys.stdout.flush()
             else:
                 self.x = None
@@ -61,11 +54,19 @@ class TilesGenerator():
             if not os.path.exists(os.path.join(self.tiles_directory, str(self.y), str(self.x)+".png")):
                 break
 
-        return (self.index, self.x, self.y)
+        return (self.x, self.y)
 
-class TilesDownloader(threading.Thread):
+    def finished(self):
+        self.index = self.index+1
+        return (self.index, self.nb_total_tiles)
+
+
+class TilesDownloader(QtCore.QThread):
+    progress = QtCore.pyqtSignal(float, str)
+
+
     def __init__(self, tiles_generator, base_url, tiles_directory):
-        threading.Thread.__init__(self)
+        QtCore.QThread.__init__(self)
         self.__stop__ = False
 
         self.tiles_generator = tiles_generator
@@ -74,94 +75,71 @@ class TilesDownloader(threading.Thread):
 
     def run(self):
         while not self.__stop__:
+            # Ask which tiles this thread should download now
             with lock:
-                (i, x, y) = self.tiles_generator.get_next()
+                (x, y) = self.tiles_generator.get_next()
             if x == None or y == None:
                 break
 
+            # Forge the URL
             url = self.base_url.replace("___y___", str(y)).replace("___x___", str(x))
+
+            # Download the image
             response = requests.get(url, stream=True)
+
+            # Save it
             with open(os.path.join(self.tiles_directory, str(y), str(x)+'.png'), 'wb') as out_file:
                 shutil.copyfileobj(response.raw, out_file)
             del response
 
+            # Check its integrity
+            try:
+                i = Image.open(os.path.join(self.tiles_directory, str(y), str(x)+'.png'))
+                del i
+            except IOError:
+                i = Image.new('RGB', (tile_width, tile_height), (255,0,0))
+                i.save(os.path.join(self.tiles_directory, str(y), str(x)+'.png'))
+
+            # Update the UI
+            with lock:
+                (index, total) = self.tiles_generator.finished()
+            self.progress.emit((0.8*index/total), "Téléchargement des tuiles {0}/{1}".format(index, total))
+            print('\b'*(len(str(total))*2+27)+"[{0}/{1}] Download".format(index, total), end="")
+
     def stop(self):
         self.__stop__ = True
 
-class Worker():
-    def __init__(self, min_x, max_x, min_y, max_y, base_url, tmp_directory, file_path, status_callback, nb_threads=4):
+
+
+
+class TilesMerger(QtCore.QThread):
+    progress = QtCore.pyqtSignal(float, str)
+
+
+    #Combine all the tiles in one single map
+    def __init__(self, min_x, max_x, min_y, max_y, tiles_directory, file_path):
+        QtCore.QThread.__init__(self)
+
         self.min_x = min_x
         self.max_x = max_x
         self.min_y = min_y
         self.max_y = max_y
-        self.base_url = base_url
-        self.tmp_directory = tmp_directory
+        self.tiles_directory = tiles_directory
         self.file_path = file_path
-        self.nb_threads = nb_threads
-        self.status_callback = status_callback
-
-        self.nb_total_tiles = (max_x-min_x+1)*(max_y-min_y+1)
-        self.tiles_generator = TilesGenerator(min_x, max_x, min_y, max_y, tmp_directory, status_callback)
-
-        self.get_all_tiles()
-        self.normalize()
-        self.combine()
-
-    def get_all_tiles(self):
-        i = 0
-        for y in range(self.min_y, self.max_y+1):
-            tmp_directory_y = os.path.join(self.tmp_directory, str(y))
-            if not os.path.exists(tmp_directory_y):
-                os.makedirs(tmp_directory_y)
-
-        threads = []
-        for i in range(0, self.nb_threads):
-            threads.append(TilesDownloader(self.tiles_generator, self.base_url, self.tmp_directory))
-            threads[i].start()
-
-        try:
-            for i in range(0, self.nb_threads):
-                threads[i].join()
-        except KeyboardInterrupt:
-            self.status_callback(0, 'Processus interompu.')
-            sys.stdout.flush()
-            for i in range(0, self.nb_threads):
-                threads[i].stop()
-                threads[i].join()
-        print('')
 
 
-
-    def normalize(self):
-        j=0
-        for y in range(self.min_y, self.max_y+1):
-            for x in range(self.min_x, self.max_x+1):
-                j=j+1
-                self.status_callback(0.6+(0.1*j/self.nb_total_tiles), "Vérification des tuiles {0}/{1}".format(j, self.nb_total_tiles))
-                print('\b'*(len(str(self.nb_total_tiles))*2+22)+"[{0}/{1}] Normalize".format(j, self.nb_total_tiles), end="")
-                sys.stdout.flush()
-                try:
-                    i = Image.open(os.path.join(self.tmp_directory, str(y), str(x)+'.png'))
-                    del i
-                except IOError:
-                    i = Image.new('RGB', (tile_width, tile_height), (255,0,0))
-                    i.save(os.path.join(self.tmp_directory, str(y), str(x)+'.png'))
-        print('')
-
-
-
-    #Combine all the tiles in one single map
-    def combine(self):
+    def run(self):
         x_composites = []
+        print('')
         for y in range(self.min_y, self.max_y+1):
-            self.status_callback(0.7+(0.2*(y-self.min_y+1)/self.max_y-self.min_y+1), "Assemblage préliminaire {0}/{1}".format(y-self.min_y+1, self.max_y-self.min_y+1))
+            self.progress.emit(0.8+(0.15*(y-self.min_y+1)/self.max_y-self.min_y+1), "Assemblage préliminaire {0}/{1}".format(y-self.min_y+1, self.max_y-self.min_y+1))
             print('\b'*(len(str(self.max_y-self.min_y+1))*2+22)+"[{0}/{1}] Pre-concatenation".format(y-self.min_y+1, self.max_y-self.min_y+1), end="")
             sys.stdout.flush()
             x_images_path = []
             for x in range(self.min_x, self.max_x+1):
-                x_images_path += [os.path.join(self.tmp_directory, str(y), str(x)+'.png')]
+                x_images_path += [os.path.join(self.tiles_directory, str(y), str(x)+'.png')]
             images = list(map(Image.open, x_images_path))
-            
+
             width = 0
             height = 0
             for i in images:
@@ -175,7 +153,7 @@ class Worker():
                 x += image.size[0]
                 image.close()
 
-        self.status_callback(0.95, "Assemblage final")
+        self.progress.emit(0.95, "Assemblage final")
         print('\n[0/1] Concatenation', end="")
         sys.stdout.flush()
         width = max(i.size[0] for i in x_composites)
@@ -189,7 +167,7 @@ class Worker():
             x_composite.close()
 
         result.save(self.file_path)
-        self.status_callback(1, "Fini")
+        self.progress.emit(1, "Fini")
         print('\b'*19+"[1/1] Concatenation")
         sys.stdout.flush()
 
